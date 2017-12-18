3296b87756a99e47ff6071af738ad17068ec4ddf
Jay M <jay@blackrimsolutions.com>
2017-12-17 18:28:15 -0700

    Working app

--- scripts.js
+++ scripts.js
@@ -17,23 +17,54 @@ $(document).ready(function () {
 
         var empName = $('#empName').val().trim();
         var role = $('#role').val().trim();
-        var startDate = $('#startDate').val().trim();
+        var startDate = moment($('#startDate').val().trim()).format("MM/DD/YY");
         var monthlyRate = $('#monthlyRate').val().trim();
-
-        dataRef.ref().push({
+        var newEmp = {
             name: empName,
             role: role,
-            startDate: startDate,
+            startDate: startDate.replace(/\//g, ''),
             monthlyRate: monthlyRate,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
-        });
-    });
-    dataRef.ref().on("child_added", function(childSnapshot){
-        console.log(childSnapshot.val().name);
-        console.log(childSnapshot.val().role);
-        console.log(childSnapshot.val().startDate);
-        console.log(childSnapshot.val().monthlyRate);
-        console.log(childSnapshot.val().dateAdded);
-    });
+            joinDate: firebase.database.ServerValue.TIMESTAMP
+        }
 
+        dataRef.ref().push(newEmp);
+
+        alert("Employee Successfully Added");
+
+        $('#empName').val("");
+        $('#role').val("");
+        $('#startDate').val("");
+        $('#monthlyRate').val("");
+    });
+    dataRef.ref().on("child_added", function (childSnapshot) {
+
+        var dbName = childSnapshot.val().name;
+        var dbRole = childSnapshot.val().role;
+        var dbStart = childSnapshot.val().startDate;
+        var dbRate = childSnapshot.val().monthlyRate;
+        var dbJoin = childSnapshot.val().joinDate;
+
+        //console.log(childSnapshot.val().name);
+        //console.log(childSnapshot.val().role);
+        //console.log(childSnapshot.val().startDate);
+        //console.log(childSnapshot.val().monthlyRate);
+        //console.log(childSnapshot.val().joinDate);
+
+        var dbStartPretty = moment.unix(dbStart).format("MM/DD/YY");
+
+        var dbMonths = moment().diff(moment.unix(dbStart, "X"), "months");
+
+        var dbBilled = dbMonths * dbRate;
+
+        $("table > tbody").append(`
+            <tr>
+                <td>${dbName}</td>
+                <td>${dbRole}</td>
+                <td>${dbStartPretty}</td>
+                <td>${dbMonths}</td>
+                <td>$${dbRate}</td>
+                <td>$${dbBilled}</td>
+            </tr>    
+        `);
+    });
 });
